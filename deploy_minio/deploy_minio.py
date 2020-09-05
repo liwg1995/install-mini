0d93bdb4a2933c5a9774bfd9e2d4b78317d0387c
olei_me <liwg@olei.me>
2020-09-05 10:52:43 +0800

    update

--- deploy_minio/deploy_minio.py
+++ deploy_minio/deploy_minio.py
@@ -21,8 +21,8 @@ def warning():
         abort(red("Only support Linux"))
     else:
         print(green("system is: "+ system_name))
-    if machine != "x86_64":
-        abort(red("Only support x86"))
+    if machine != "arrch64":
+        abort(red("Only support huawei kunpeng arrch64"))
     else:
         print(green("framework is: " + machine))
     if os_name != "CentOS Linux":

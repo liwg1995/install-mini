6f85b73010b0debc35d714da6672a40aba86de52
olei_me <liwg@olei.me>
2020-09-12 09:40:59 +0800

    update script

--- install_minio.sh
+++ install_minio.sh
@@ -73,7 +73,7 @@ if [ $? == 0 ]; then
 else
   echo "The operating system is not networked! Install fabric3 from local"
   python3_ver=$(python3 --version | awk -F ' ' '{print $2}' | awk -F '.' '{print $1$2}')
-  # 这部分，离线安装还有些BUG
+  # 这修改完毕，待测试
   if [ $python3_ver == '36' ]; then
     pip3 install --no-index --find-links=./python-packages/kunpeng/python3.6/ fabric3
   elif [ $python3_ver == '37' ]; then
